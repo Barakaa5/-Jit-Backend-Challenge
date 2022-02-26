0f88e4c80222c832545b012007cab8401036450b
Barak <barak.amzaleg@tutoit.io>
2022-02-26 20:30:32 +0200

    Second commit

--- app.js
+++ app.js
@@ -3,25 +3,23 @@ const prompt = require("prompt-sync")();
 const shell = require("shelljs");
 
 let repositories = [];
-async function GitHubAPI() {
+async function GitHubAPI(path) {
   let response = await axios.get(path);
   return response.data.items;
 }
 
-let numRepo = parseInt(prompt("Welcome, How many repositories to search? "));
+let numRepo = parseInt(prompt("Welcome, How many repositories to search? ")); // Asking from the user the amount of repo to check
 while (!Number.isInteger(numRepo))
   numRepo = parseInt(prompt("Please insert an integer: "));
-let n;
-if (numRepo < 100) n = 1;
-else n = numRepo % 100;
+if (numRepo > 100) numRepo = 100;// Limit the search 
 
-let path = `https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=${numRepo}&page=${n}`;
+let query = `q=language:javascript&sort=stars&order=desc&per_page=${numRepo}&page=0`;
 
+let path = `https://api.github.com/search/repositories?${query}`;
 
-GitHubAPI()
+GitHubAPI(path)
   .then(function (response) {
-    console.log("repositories: " + response.length);
-    response.forEach((repository) => {
+    response.forEach((repository) => {// createing an array with relevant data of the repositories
       repositories.push({
         repoName: repository.name,
         URL: repository.html_url,
@@ -29,21 +27,21 @@ GitHubAPI()
       });
     });
     shell.cd("./repositories");
-    repositories.forEach(function (repo) {
+    repositories.forEach(function (repo) {// for Each repo, clone it, calculate it's Security Score and remove the cloning folder.
       shell.exec(`git clone ${repo.URL}`);
       shell.cd(`./${repo.repoName}`);
       let text = shell.exec("depcheck").stdout;
       let counter = 0;
-      let Missing = text.indexOf("Missing dependencies");
-      let length = (Missing != -1)? Missing : text.length; 
+      let Missing = text.indexOf("Missing dependencies");//ignoring Missing dependencies
+      let length = Missing != -1 ? Missing : text.length;
       for (let i = 0; i < length; i++) if (text[i] == "*") counter++;
       shell.cd(`..`);
       shell.exec(`rm -r ${repo.repoName}`);
-      repo.Security_Score = 100 - counter;
+      repo.Security_Score = 100 - counter; //Security_Score is (100 - the amount of Unused devDependencies)
     });
     console.log("=========================================");
-    console.log("================ Summary ================");
-    repositories.sort((a,b) => b.Security_Score - a.Security_Score);
+    console.log("================ Summary ================"); //output Summary
+    repositories.sort((a, b) => b.Security_Score - a.Security_Score);
     repositories.forEach(function (repo) {
       console.log("--------------------------------");
       console.log("Repository Name: " + repo.repoName);
@@ -54,6 +52,5 @@ GitHubAPI()
     });
     console.log("================== End ==================");
     console.log("=========================================");
-
   })
   .catch((error) => console.log(error));
